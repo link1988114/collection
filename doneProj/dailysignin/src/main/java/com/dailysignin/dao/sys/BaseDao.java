688a050cc254222a3bb000c90ba8855f40b3a481
lissiana <lissiana@google.com>
2020-03-09 12:29:57 +0800

    update

--- doneProj/dailysignin/src/main/java/com/dailysignin/dao/sys/BaseDao.java
+++
@@ -1,9 +0,0 @@
-package com.dailysignin.dao.sys;
-
-import com.dailysignin.dao.SQLgetter;
-
-public class BaseDao
-{
-	protected MybatisConnection db = new MybatisConnection();
-	protected SQLgetter sqls = SQLgetter.getInstance();
-}
--- doneProj/vvote/src/main/java/com/vvote/dao/sys/BaseDao.java
+++
@@ -1,9 +0,0 @@
-package com.vvote.dao.sys;
-
-import com.vvote.dao.SQLgetter;
-
-public class BaseDao
-{
-	protected MybatisConnection db = new MybatisConnection();
-	protected SQLgetter sqls = SQLgetter.getInstance();
-}

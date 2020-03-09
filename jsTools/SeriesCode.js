a6facb40537c44a74a6587a5813aa105b5cd040d
lissiana <lissiana@google.com>
2020-03-09 09:48:33 +0800

    js tools set

---
+++ jsTools/SeriesCode.js
@@ -0,0 +1,98 @@
+﻿function seriesCode(rule_input, from_input, to_input) {
+    var numLength = rule_input.length - rule_input.replace(/\*/g, "").length;
+
+    var placemark = "";
+    for (var i = 0; i < numLength; i++) {
+        placemark += "0";
+    }
+
+    var seriesArr = new Array();
+    for (var i = from_input; i <= to_input; i++) {
+        var value = placemark + i;
+        value = value.substr(value.length - numLength, value.length);
+
+        var temp = "";
+        var index = 0;
+        for (var j = 0; j < rule_input.length; j++) {
+            if (rule_input.charAt(j) == "*") {
+                temp += value.charAt(index);
+                index++;
+            }
+            else {
+                temp += rule_input.charAt(j);
+            }
+        }
+        temp += Math.floor(Math.random() * 10); //random ending
+        seriesArr.push(temp);
+    }
+
+    return seriesArr;
+}
+
+function rndCode(totalitems, type, length) {
+    var resultarr = new Array();
+    var pool = new Array();
+    //acsii   0~9 : 48~57   a~z : 97~122   A~Z : 65~90
+
+    if (type == "num") {
+        for (var i = 48; i <= 57; i++) {
+            pool.push(String.fromCharCode(i));
+        }
+    }
+    if (type == "alphabet") {
+        for (var i = 97; i <= 122; i++) {
+            pool.push(String.fromCharCode(i));
+        }
+    }
+    if (type == "Alphabet") {
+        for (var i = 65; i <= 90; i++) {
+            pool.push(String.fromCharCode(i));
+        }
+    }
+    if (type == "numalphabet") {
+        for (var i = 48; i <= 57; i++) {
+            pool.push(String.fromCharCode(i));
+        }
+        for (var i = 97; i <= 122; i++) {
+            pool.push(String.fromCharCode(i));
+        }
+    }
+    if (type == "numAlphabet") {
+        for (var i = 48; i <= 57; i++) {
+            pool.push(String.fromCharCode(i));
+        }
+        for (var i = 65; i <= 90; i++) {
+            pool.push(String.fromCharCode(i));
+        }
+    }
+    if (type == "Alphabetalphabet") {
+        for (var i = 97; i <= 122; i++) {
+            pool.push(String.fromCharCode(i));
+        }
+        for (var i = 65; i <= 90; i++) {
+            pool.push(String.fromCharCode(i));
+        }
+    }
+    if (type == "numAlphabetalphabet") {
+        for (var i = 48; i <= 57; i++) {
+            pool.push(String.fromCharCode(i));
+        }
+        for (var i = 97; i <= 122; i++) {
+            pool.push(String.fromCharCode(i));
+        }
+        for (var i = 65; i <= 90; i++) {
+            pool.push(String.fromCharCode(i));
+        }
+    }
+
+    for (var i = 0; i < totalitems; i++) {
+        var temp = "";
+        for (var j = 0; j < length; j++) {
+            var pick = pool[Math.floor(Math.random() * pool.length)];
+            temp += pick;
+        }
+        resultarr.push(temp);
+    }
+
+    return resultarr;
+}
---
+++ jsTools/pagination.js
@@ -0,0 +1,155 @@
+﻿var Pagination = {
+    createNew: function () {
+        var pagination = {};
+
+        pagination.containerObj = new Object();
+        pagination.navObj = new Object();
+        pagination.data = { data: [] };
+        pagination.pageItems = 2;
+        pagination.navLength = 5;
+        pagination.maxPage = 0;
+        pagination.currPage = 1;
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+        pagination.initial = function (containerObj, navObj, data, pageItems, navLength) {
+            pagination.containerObj = containerObj;
+            pagination.navObj = navObj;
+            pagination.data = data;
+            pagination.pageItems = pageItems;
+            pagination.navLength = navLength <= 0 ? 1: navLength;
+            pagination.maxPage = Math.ceil(pagination.data.data.length / pagination.pageItems);
+            if (this.maxPage < this.navLength) {
+                this.navLength = this.maxPage;
+            }
+
+            pagination.currPage = 1;
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+        pagination.getTotalPage = function () {
+            return Math.ceil(pagination.data.data.length / pagination.pageItems);
+        }
+
+        pagination.getPageData = function (pageNo) {
+            var start = (pageNo - 1) * pagination.pageItems;
+            var end = pageNo * pagination.pageItems > pagination.data.data.length ? pagination.data.data.length : pageNo * pagination.pageItems;
+            var partialData = {
+                data: []
+            };
+            for (var i = start; i < end; i++) {
+                partialData.data.push(pagination.data.data[i]);
+            }
+            return partialData;
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+        pagination.updateView = function(pageNo) {
+            var pageData = pagination.getPageData(pageNo);
+
+            pagination.createView(pageData);
+            pagination.createNavigation(pageNo);
+        }
+
+        pagination.createView = function (pageData) {
+            var htmlstr = "<tr id=\"hint_table_body\">" +
+                                "<th>车牌</th>" +
+                                "<th>姓名</th>" +
+                                "<th>出单时间</th>" +
+                                "<th>发展人</th>" +
+                      "</tr>";
+
+            for (var i = 0; i < pageData.data.length; i++) {
+                htmlstr += "<tr>" +
+                                        "<td align=\"center\" width=\"60\">" + pageData.data[i].chepai + "<br></td>" +
+                                        "<td align=\"center\" width=\"60\">" + pageData.data[i].xingming + "</td>" +
+                                        "<td align=\"center\" width=\"65\">" + pageData.data[i].xunjiashijian + "</td>" +
+                                        "<td align=\"center\">" + pageData.data[i].khjl + "</td>" +
+                                    "</tr>";
+            }
+
+            try {
+                this.containerObj.innerHTML = htmlstr;
+            }
+            catch (e) {
+                var div = document.createElement("div");
+                div.innerHTML = "<table>"+htmlstr+"</table>";
+                this.containerObj.replaceChild(div.firstChild.firstChild, this.containerObj.firstChild);
+            }
+            
+        }
+
+        pagination.createNavigation = function (pageNo) {
+            var half = Math.floor(pagination.navLength / 2);
+            var currIndex = 0;
+            pagination.currPage = pageNo;
+
+            var resultArr = new Array();
+            for (var i = pageNo - half; i < pageNo; i++) {
+                resultArr.push(i);
+            }
+            currIndex = resultArr.length;
+            for (var i = pageNo; i < pageNo + (pagination.navLength - half) ; i++) {
+                resultArr.push(i);
+            }
+            if (resultArr[0] <= 0) {
+                currIndex = pageNo - 1;
+                for (i = 0; i < resultArr.length; i++) {
+                    resultArr[i] = i + 1;
+                }
+            }
+            if (resultArr[resultArr.length - 1] > pagination.maxPage) {
+                currIndex = pageNo - (pagination.maxPage - pagination.navLength) - 1;
+                for (i = 0; i < resultArr.length; i++) {
+                    resultArr[i] = pagination.maxPage - pagination.navLength + 1 + i;
+                }
+            }
+
+            var htmlstr = ""
+            for(i=0;i<resultArr.length;i++){
+                if(i==currIndex){
+                    htmlstr += "<li class=\"currpage\" onclick=\"updateView("+resultArr[i]+")\">"+resultArr[i]+"</li>";
+                }
+                else{
+                    htmlstr += "<li onclick=\"updateView(" + resultArr[i] + ")\">" + resultArr[i] + "</li>";
+                }
+            }
+
+            pagination.navObj.innerHTML = htmlstr;
+        }
+
+        pagination.gotoPage = function (pageNo) {
+            pagination.currPage = Number(pageNo);
+            if (pagination.currPage <= 0) {
+                pagination.currPage = 1;
+            }
+            if (pagination.currPage >= pagination.maxPage) {
+                pagination.currPage = pagination.maxPage;
+            }
+            
+            pagination.updateView(pagination.currPage);
+        }
+
+        pagination.next = function() {
+            pagination.currPage++;
+            if (pagination.currPage >= pagination.maxPage) {
+                pagination.currPage = pagination.maxPage;
+            }
+            pagination.updateView(pagination.currPage);
+        }
+
+        pagination.pre = function() {
+            pagination.currPage--;
+            if (pagination.currPage <= 1) {
+                pagination.currPage = 1;
+            }
+            pagination.updateView(pagination.currPage);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+        return pagination;
+    }
+};
